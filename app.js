5db3ca214358e524cef78a4a459798985e78b52b
Mr.SoftyXTrillion <123297701+MrSoftyXTrillion@users.noreply.github.com>
2023-07-08 10:54:11 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+$(function(){
+    //BANNER SLICK SLIDER STARTS HERE
+    $('#banner').slick({
+        arrows:false,
+        dots:true,
+        autoplay:true
+      });
+    //BANNER SLICK SLIDER ENDS HERE
+})
